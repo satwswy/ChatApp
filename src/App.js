c67ce2727ebac03b7ab67b60afdcf1c4281d2ef4
marios <mgeorgak859@gmail.com>
2022-09-23 12:43:27 +0300

    adds

--- src/App.js
+++ src/App.js
@@ -3,21 +3,35 @@ import "./App.css";
 import ContactListComponent from "./components/ContactList/ContactListComponent";
 import ConversationComponent from "./components/Conversation/ConversationComponent";
 import React, { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import RegisterPage from "./components/RegisterPage/RegisterPage";
 
 function App() {
   const [selectedChat, setSelectedChat] = useState();
   return (
-    <div className="Container">
-      <ContactListComponent setChat={setSelectedChat}/>
-      {selectedChat ? (
-        <ConversationComponent selectedChat={selectedChat}/>
-      ) : (
-        <div className="Placeholder">
-          <img className="ChatPlaceholder" src="https://insuranceworld.gr/wp-content/uploads/2021/01/FsWUqRoOsPu.png" alt="whatsapp"/>
-          <span className="spanspan">whatsapp</span>
-        </div>
-      )}
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<RegisterPage/>}/>
+        <Route path="/homepage" element={<div className="Container">
+          <ContactListComponent setChat={setSelectedChat} />
+          {selectedChat ? (
+            <ConversationComponent selectedChat={selectedChat} />
+          ) : (
+            <div className="Placeholder">
+              <img
+                className="ChatPlaceholder"
+                src="https://insuranceworld.gr/wp-content/uploads/2021/01/FsWUqRoOsPu.png"
+                alt="whatsapp"
+              />
+              <span className="spanspan">whatsapp</span>
+            </div>
+          )}
+        </div>}/>
+
+        
+        
+      </Routes>
+    </BrowserRouter>
   );
 }
 
